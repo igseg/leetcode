8ab7d1d3f3d84472289ffa51340098f34cbfe182
093C467E3 <ignacio.s.tamarit@gmail.com>
2022-11-06 17:26:01 +0100

    Problem Two sum solved using numpy, better solution

--- 1.TwoSum/twosum.py
+++ 1.TwoSum/twosum.py
@@ -11,19 +11,14 @@ import numpy as np
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for idx, elem in enumerate(nums):
-            aux_nums = nums[:idx] + nums[idx+1:]
-            if elem < target - np.max(aux_nums):
-                continue
 
-            aux_nums  = np.array(aux_nums) + elem
-            condition = (aux_nums  == target)
+            aux_nums = nums[idx+1:]
+
+            condition = (np.array(aux_nums) == (target - elem))
 
             if np.sum(condition) == 0:
                 continue
 
             idx_2 = np.where(condition)[0][0]
 
-            if idx_2 >= idx:
-                idx_2 += 1
-
-            return [idx, idx_2]
+            return [idx, idx_2+1+idx]
