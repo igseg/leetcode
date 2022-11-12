b28c591310aaee7da733d041c88e2110b0f73a96
093C467E3 <ignacio.s.tamarit@gmail.com>
2022-11-12 14:54:26 +0100

    palindrom number check

---
+++ 9.Palindrome_number/palindrome_number.py
@@ -0,0 +1,18 @@
+# 9. Palindrome Number
+# Easy
+
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        if x < 0:
+            return False
+
+        x = str(x)
+
+        for i in range(int(len(x)/2)):
+            if x[i] != x[-(i+1)]:
+                return False
+
+        return True
